244518084ee0030d171166af514912c1161baafc
Sanjay Pahari <sanjay@dvorakinnovation.com>
2023-06-01 13:05:26 +0545

    reactapp

--- src/Components/SubmitForm/SubmitForm.tsx
+++ src/Components/SubmitForm/SubmitForm.tsx
@@ -1,21 +1,31 @@
+import React from "react";
 import axios from "axios";
-import { FormData } from "./SubmitForm.model";
-import { FormContainer, InputField, SubmitButton, TitleDiv , DesciptionDiv } from "./Submitform.styled";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
+import {
+  FormContainer,
+  InputField,
+  SubmitButton,
+  TitleDiv,
+  DesciptionDiv,
+} from "./Submitform.styled";
 
+interface FormData {
+  title: string;
+  description: string;
+}
 
 const SubmitForms = () => {
-  const { handleSubmit, register } = useForm<FormData>();
+  const { handleSubmit, register, reset } = useForm<FormData>();
 
-  const onSubmit: SubmitHandler<FormData> = (formData) => {
+  const onSubmit = (formData: FormData) => {
     axios
-      .post("http://localhost:3000/forms", formData)
-      .then((forms) => {
-        console.log("Form submitted successfully", forms.data);
+      .post("http://localhost:3000/form", formData)
+      .then((response) => {
+        console.log("Form submitted successfully", response.data);
+        reset(); 
       })
       .catch((error) => {
-        
         console.error("Error submitting form", error);
       });
   };
@@ -24,13 +34,18 @@ const SubmitForms = () => {
     <FormContainer>
       <form onSubmit={handleSubmit(onSubmit)}>
         <TitleDiv> TITLE </TitleDiv>
-        <InputField type="text" placeholder="TITLE" {...register("title", { required: true })} />
-<DesciptionDiv></DesciptionDiv>
+        <InputField
+          type="text"
+          placeholder="TITLE"
+          {...register("title", { required: true })}
+        />
+        <DesciptionDiv>DESCRIPTION</DesciptionDiv>
         <InputField
           type="text"
           placeholder="DESCRIPTION"
-          {...register("title", { required: true })}
+          {...register("description", { required: true })}
         />
+
         <SubmitButton type="submit">Submit</SubmitButton>
       </form>
     </FormContainer>
